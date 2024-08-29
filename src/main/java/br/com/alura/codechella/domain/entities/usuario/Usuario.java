072f31a2e188263166b22bfdeafcfff17ea25c48
LucasFrancoBN <126281004+scl-lnavarro@users.noreply.github.com>
2024-08-29 16:00:06 -0300

    objetos de valor e fábrica de usuários

--- src/main/java/br/com/alura/codechella/domain/entities/usuario/Usuario.java
+++ src/main/java/br/com/alura/codechella/domain/entities/usuario/Usuario.java
@@ -1,5 +1,7 @@
 package br.com.alura.codechella.domain.entities.usuario;
 
+import br.com.alura.codechella.domain.Endereco;
+
 import java.time.LocalDate;
 
 public class Usuario {
@@ -8,6 +10,7 @@ public class Usuario {
     private String nome;
     private LocalDate nascimento;
     private String email;
+    private Endereco endereco;
 
     public Usuario() {
     }
@@ -61,5 +64,13 @@ public class Usuario {
 
     public void setEmail(String email) {
         this.email = email;
+    }
+
+    public Endereco getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(Endereco endereco) {
+        this.endereco = endereco;
     }
 }
