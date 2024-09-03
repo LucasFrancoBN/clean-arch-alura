259c2e262f65c7fb6c75afa5e82de3ba5a77aeaf
LucasFrancoBN <126281004+scl-lnavarro@users.noreply.github.com>
2024-09-03 15:35:30 -0300

    camada de infra

---
+++ src/main/java/br/com/alura/codechella/infra/gateways/RepositorioDeUsuarioJpa.java
@@ -0,0 +1,30 @@
+package br.com.alura.codechella.infra.gateways;
+
+import br.com.alura.codechella.application.gateways.RepositorioDeUsuario;
+import br.com.alura.codechella.domain.entities.usuario.Usuario;
+import br.com.alura.codechella.infra.persistence.UsuarioEntity;
+import br.com.alura.codechella.infra.persistence.UsuarioRepository;
+
+import java.util.List;
+
+public class RepositorioDeUsuarioJpa implements RepositorioDeUsuario {
+    private final UsuarioRepository repositorio;
+    private final UsuarioEntityMapper mapper;
+
+    public RepositorioDeUsuarioJpa(UsuarioRepository repositorio, UsuarioEntityMapper mapper) {
+        this.repositorio = repositorio;
+        this.mapper = mapper;
+    }
+
+    @Override
+    public Usuario cadastrarUsuario(Usuario usuario) {
+        UsuarioEntity usuarioEntity = mapper.toEntity(usuario);
+        return mapper.toDomain(repositorio.save(usuarioEntity));
+    }
+
+    @Override
+    public List<Usuario> listarTodos() {
+        // return repositorio.findAll();
+        return null;
+    }
+}
